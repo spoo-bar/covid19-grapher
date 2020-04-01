c5df61387c624399200a29c5594b8ae63fc0b400
Spoorthi S <spoon611@users.noreply.github.com>
2020-04-01 13:14:15 +0530

    formating death rate chart

--- src/components/DeathRateTable.vue
+++ src/components/DeathRateTable.vue
@@ -1,21 +1,51 @@
 <template>
   <div class="death-rate-table">
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">Rank</th>
-          <th scope="col">Name</th>
-          <th scope="col">Death Rate</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="country in displayData" :key="country.name">
-          <th scope="row">#{{ country.rank }}</th>
-          <td>{{ country.name }}</td>
-          <td>{{ country.deathRate }}%</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="row">
+      <table v-if="displayData1.length > 0" class="table col-lg-6">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Rank</th>
+            <th scope="col">Death Rate</th>
+            <th scope="col">Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="country in displayData1" :key="country.name">
+            <th scope="row">#{{ country.rank }}</th>
+            <td>{{ country.deathRate }}%</td>
+            <td>
+              {{ country.name }}
+              <span
+                class="badge badge-pill badge-secondary"
+                v-show="country.name === defaultCountry"
+              >Default</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table v-if="displayData2.length" class="table col-lg-6">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Rank</th>
+            <th scope="col">Death Rate</th>
+            <th scope="col">Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="country in displayData2" :key="country.name">
+            <th scope="row">#{{ country.rank }}</th>
+            <td>{{ country.deathRate }}%</td>
+            <td>
+              {{ country.name }}
+              <span
+                class="badge badge-pill badge-secondary"
+                v-show="country.name === defaultCountry"
+              >Default</span>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -28,7 +58,9 @@ export default {
     return {
       data: Data,
       tableData: [],
-      displayData: []
+      displayData1: [],
+      displayData2: [],
+      defaultCountry: ""
     };
   },
   methods: {
@@ -54,21 +86,39 @@ export default {
       this.tableData.sort(
         (a, b) => parseFloat(b.deathRate) - parseFloat(a.deathRate)
       );
+    },
+    getDefaultCountry: function() {
+      let defaultCountry = localStorage.getItem("defaultCountry");
+      if (defaultCountry) {
+        return defaultCountry;
+      } else {
+        return "World";
+      }
     }
   },
   mounted: function() {
-
-      this.loadData();
+    this.loadData();
     for (let i = 1; i <= 10; i++) {
-      this.displayData.push({
+      this.displayData1.push({
         rank: i,
-        name: this.tableData[i].name,
-        deathRate: this.tableData[i].deathRate,
-        confirmedCases: this.getCountryData(this.tableData[i].name)
+        name: this.tableData[i-1].name,
+        deathRate: this.tableData[i-1].deathRate,
+        confirmedCases: this.getCountryData(this.tableData[i-1].name)
           .TotalCasesCount,
-        deathCases: this.getCountryData(this.tableData[i].name).TotalDeathsCount
+        deathCases: this.getCountryData(this.tableData[i-1].name).TotalDeathsCount
       });
     }
+    for (let i = 11; i <= 20; i++) {
+      this.displayData2.push({
+        rank: i,
+        name: this.tableData[i-1].name,
+        deathRate: this.tableData[i-1].deathRate,
+        confirmedCases: this.getCountryData(this.tableData[i-1].name)
+          .TotalCasesCount,
+        deathCases: this.getCountryData(this.tableData[i-1].name).TotalDeathsCount
+      });
+    }
+    this.defaultCountry = this.getDefaultCountry();
   }
 };
 </script>
