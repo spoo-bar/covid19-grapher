9894350737f4ed94d81f93047b3789234d9f3570
Spoorthi S <spoon611@users.noreply.github.com>
2020-04-01 13:15:22 +0530

    fixing chart tooltip text error

--- src/components/DeadPatientPercentageChart.vue
+++ src/components/DeadPatientPercentageChart.vue
@@ -43,7 +43,7 @@ export default {
         },
         series: [
           {
-            name: "Confirmed Cases",
+            name: "Confirmed Deaths",
             colorByPoint: true,
             data: []
           }
