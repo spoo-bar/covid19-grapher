ef19fd01c8f1ad454b996837cf47efece90af1a4
Vignesh P <vignesh.p1110@gmail.com>
2020-04-11 13:33:40 +0530

    modified:   src/get_files.py

--- src/get_files.py
+++ src/get_files.py
@@ -5,12 +5,10 @@ with open('url_list.txt', 'r') as urls:
     for csv_url in urls:
         csv_url = csv_url.strip()
         filename = csv_url.split('/')[-1]
-
         try:
             with requests.get(csv_url) as response:
                 url_content = response.content
                 with open(filename, 'wb') as csv_file:
-                    csv_file.write(url_content)
-                
+                    csv_file.write(url_content)               
         except RequestException as err:
             print('Downloading {csv_url} failed due to {err}')
