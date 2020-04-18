7788990f3949e8bb02800553bb52c6221f527193
Vignesh P <vignesh.p1110@gmail.com>
2020-04-18 16:50:50 +0530

    modified:   create_json.py     modified code only to use 2 data files, generating totals from them

--- src/create_json.py
+++ src/create_json.py
@@ -1,13 +1,6 @@
 import pandas as pd
-
-"""
-locations_url = "https://covid.ourworldindata.org/data/ecdc/locations.csv"
-try:
-    LocationData = pd.read_csv("locations.csv", index_col=False, usecols=['location', 'continent', 'population'])
-except BaseException as err:
-    print(f'Downloading {locations_url} failed: {str(err)}')
-    exit
-"""
+import datetime
+import json
 
 LocationData = pd.read_csv("locations.csv", index_col=False, usecols=['location', 'continent', 'population'])
 LocationData.drop_duplicates(subset='location', inplace=True)
@@ -26,7 +19,7 @@ class LocationClass:
     world_population = 0
 
     def __init__(self, location, continent, population):
-        #Object constructor for the class
+        #Object constructor for the class Location
 
         self.Name = location
         self.Continent = continent
@@ -35,30 +28,68 @@ class LocationClass:
             LocationClass.world_population += self.Population        
         except:
             self.Population = None
+              
+    def AddData(self, CasesData, DeathsData):
+        #Function to add the output dataset to the location object
+
+        setattr(self, "Cases", CasesData)
+        setattr(self, "Deaths", DeathsData)
+
+
+def evalData(Count_arr):
+   #Function to evaluate the output dataset
+
+    tempDate = datetime.datetime(2019, 12, 31)
+    result = dict()
+    result["DayCount"] = list()
+    result["DayCountSinceFirst"] = list()
+    result["SevenDayAvg"] = list()
+    result["SevenDayAvgSinceFirst"] = list()
+    result["Total"] = list()
+    result["TotalSinceFirst"] = list()
+    found = False
+    for count in Count_arr:
+        if not count:
+            count = 0.0
+        if found:               
+            result["DayCountSinceFirst"].append(count)
+            result["TotalSinceFirst"].append(sum(result["DayCountSinceFirst"]))
+            result["SevenDayAvgSinceFirst"].append(result["TotalSinceFirst"][-1]/7)
+        else:
+            if count:
+                found = True
+                result["FirstDate"] = tempDate.strftime('%Y-%m-%dT%H:%M:%S')
+                result["DayCountSinceFirst"].append(count)
+                result["TotalSinceFirst"].append(sum(result["DayCountSinceFirst"]))
+                result["SevenDayAvgSinceFirst"].append(result["TotalSinceFirst"][-1]/7)
+        result["DayCount"].append(count)
+        result["Total"].append(sum(result["DayCount"]))
+        result["SevenDayAvg"].append(result["Total"][-1]/7)
+        tempDate += datetime.timedelta(days=1)
+
+    return result
             
-    
-    def create_attribute(self, label):
-        setattr(self, label, {})
-        #self.lable["DayCountSinceFirstOccurrence"] =
-        #self.lable["DayCount"] =
-        #self.lable["SevenDayAvgSinceFirstOccurrence"] =
-        #self.lable["SevenDayAvg"] =
-
-
-class DataClass:
-    #Object class for the data elements (DayCount, DayCountSinceFirstOccurrence, SevenDayAvg, SevenDayAvgSinceFirstOccurrence)
-
-    def __init__(self, DayCount):
-        self.DayCount = DayCount
-
 
 object_list = list( LocationClass(row.location, row.continent, row.population) for index, row in LocationData.iterrows())
-newCases = pd.read_csv("new_cases.csv", index_col=False)
+Cases_csv = pd.read_csv("new_cases.csv", index_col=False)
+Cases_csv.fillna(0, inplace=True)
+Deaths_csv = pd.read_csv("new_deaths.csv", index_col=False)
+Deaths_csv.fillna(0, inplace=True)
 
-for index, row in LocationData.iterrows():
-    location = row.location
-    print(location)
-    temp_arr = [r[location] for i, r in newCases.iterrows()]
-    daycount = DataClass(temp_arr)
+output = dict()
+output["Source"] = "https://ourworldindata.org/coronavirus-source-data"
+output["LastUpdateTime"] = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+output["Data"] = list()
+for place in object_list:
+    location = place.Name
+    Cases_arr = [r[location] for i, r in Cases_csv.iterrows()]
+    CasesData = evalData(Cases_arr)
+    Deaths_arr = [r[location] for i, r in Deaths_csv.iterrows()]
+    DeathsData = evalData(Deaths_arr)
+    place.AddData(CasesData, DeathsData)
+    output["Data"].append(vars(place))
 
-print(vars(daycount))
+
+
+with open('data.json', 'w') as output_file:
+    json.dump(output, output_file)
