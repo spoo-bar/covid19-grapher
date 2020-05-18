b2901f23004d6148fc23b6a4a2b0a4aaa42e20b5
Vignesh P <vignesh.p1110@gmail.com>
2020-05-18 15:54:24 +0530

    addressed comments from p33ves changes 09/05 #8

--- data/download_source_data.py
+++ data/download_source_data.py
@@ -1,5 +1,6 @@
-import requests
 import os
+
+import requests
 from requests.exceptions import RequestException
 
 temp_folder = "tmp"
@@ -7,18 +8,18 @@ os.chdir("data")
 try:
     os.mkdir(temp_folder)
 except OSError:
-    print (f"Creation of the directory {temp_folder} failed")
+    print(f"Creation of the directory {temp_folder} failed")
     exit(1)
 
 
-with open('url_list.txt', 'r') as urls:
+with open("data_source_urls.txt", "r") as urls:
     for csv_url in urls:
         csv_url = csv_url.strip()
-        filename = temp_folder+"/"+csv_url.split('/')[-1]
+        filename = temp_folder + "/" + csv_url.split("/")[-1]
         try:
             with requests.get(csv_url) as response:
                 url_content = response.content
-                with open(filename, 'wb') as csv_file:
-                    csv_file.write(url_content)               
+                with open(filename, "wb") as csv_file:
+                    csv_file.write(url_content)
         except RequestException as err:
             print(f"Downloading {csv_url} failed due to {err}")
--- data/generate_data_json.py
+++ data/generate_data_json.py
@@ -1,25 +1,30 @@
-import pandas as pd
 import datetime
 import json
 import shutil
 
+import pandas as pd
+
 temp_folder = "./data/tmp"
-LocationData = pd.read_csv(temp_folder+"/locations.csv", index_col=False, usecols=['location', 'continent', 'population'])
-LocationData.drop_duplicates(subset='location', inplace=True)
-LocationData.sort_values(by=['location'], inplace=True)
-temp_row = pd.Series({'location':"World", 'continent':None, 'population':None})
+LocationData = pd.read_csv(
+    temp_folder + "/locations.csv",
+    index_col=False,
+    usecols=["location", "continent", "population"],
+)
+LocationData.drop_duplicates(subset="location", inplace=True)
+LocationData.sort_values(by=["location"], inplace=True)
+temp_row = pd.Series({"location": "World", "continent": None, "population": None})
 temp_df = pd.DataFrame([temp_row])
 LocationData = pd.concat([temp_df, LocationData], ignore_index=True)
-LocationData.fillna("null", inplace = True)
+LocationData.fillna("null", inplace=True)
 
 
-class LocationClass:        
-    #Object class of each country in the locations.csv file
-      
+class LocationClass:
+    # Object class of each country in the locations.csv file
+
     world_population = 0
 
     def __init__(self, location, continent, population):
-        #Object constructor for the class Location
+        # Object constructor for the class Location
 
         self.Name = location
         if continent == "null":
@@ -28,12 +33,14 @@ class LocationClass:
             self.Continent = continent
         try:
             self.Population = int(population)
-            LocationClass.world_population += self.Population        
+            LocationClass.world_population += self.Population
         except:
             self.Population = None
-              
-    def AddData(self, CasesData, DeathsData, FirstCase, FirstDeath, TotalCases, TotalDeaths):
-        #Function to add the output dataset to the location object
+
+    def AddData(
+        self, CasesData, DeathsData, FirstCase, FirstDeath, TotalCases, TotalDeaths
+    ):
+        # Function to add the output dataset to the location object
 
         setattr(self, "Cases", CasesData)
         setattr(self, "Deaths", DeathsData)
@@ -44,50 +51,55 @@ class LocationClass:
 
 
 def evalData(Count_arr):
-   #Function to evaluate the output dataset
+    # Function to evaluate the output dataset
 
     DateCounter = datetime.datetime(2019, 12, 31)
     result = dict()
-    result["DayCount"] = list()   
-    result["SevenDayAvg"] = list()  
+    result["DayCount"] = list()
+    result["SevenDayAvg"] = list()
     result["TotalCount"] = list()
     found = False
     for index, count in enumerate(Count_arr):
         if not count:
             count = 0
-        if count and not found:               
+        if count and not found:
             found = True
             FirstDate = DateCounter
         result["DayCount"].append(count)
         result["TotalCount"].append(sum(result["DayCount"]))
         CurrentTotal = result["TotalCount"][-1]
         if index < 7:
-            result["SevenDayAvg"].append(round(CurrentTotal/7, 3))
+            result["SevenDayAvg"].append(round(CurrentTotal / 7, 3))
         else:
-            result["SevenDayAvg"].append(round(sum(Count_arr[index-6:index+1])/7, 3))
+            result["SevenDayAvg"].append(
+                round(sum(Count_arr[index - 6 : index + 1]) / 7, 3)
+            )
         DateCounter += datetime.timedelta(days=1)
     try:
-        DayDifference = (FirstDate -  datetime.datetime(2019, 12, 31)).days
-        FirstDate_str = FirstDate.strftime('%Y-%m-%dT%H:%M:%S')
+        DayDifference = (FirstDate - datetime.datetime(2019, 12, 31)).days
+        FirstDate_str = FirstDate.strftime("%Y-%m-%dT%H:%M:%S")
     except:
         DayDifference = -1
         FirstDate_str = None
-    result["DayCountSinceFirst"] = result["DayCount"][DayDifference : ]
-    result["SevenDayAvgSinceFirst"] = result["SevenDayAvg"][DayDifference : ]
-    result["TotalCountSinceFirst"] = result["TotalCount"][DayDifference : ]
+    result["DayCountSinceFirst"] = result["DayCount"][DayDifference:]
+    result["SevenDayAvgSinceFirst"] = result["SevenDayAvg"][DayDifference:]
+    result["TotalCountSinceFirst"] = result["TotalCount"][DayDifference:]
     return (FirstDate_str, result, CurrentTotal)
-            
 
-object_list = list( LocationClass(row.location, row.continent, row.population) for index, row in LocationData.iterrows())
+
+object_list = list(
+    LocationClass(row.location, row.continent, row.population)
+    for index, row in LocationData.iterrows()
+)
 object_list[0].Population = LocationClass.world_population
-Cases_csv = pd.read_csv(temp_folder+"/new_cases.csv", index_col=False)
+Cases_csv = pd.read_csv(temp_folder + "/new_cases.csv", index_col=False)
 Cases_csv.fillna(0, inplace=True)
-Deaths_csv = pd.read_csv(temp_folder+"/new_deaths.csv", index_col=False)
+Deaths_csv = pd.read_csv(temp_folder + "/new_deaths.csv", index_col=False)
 Deaths_csv.fillna(0, inplace=True)
 
 output = dict()
 output["Source"] = "https://ourworldindata.org/coronavirus-source-data"
-output["LastUpdateTime"] = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+output["LastUpdateTime"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 output["Data"] = list()
 for place in object_list:
     location = place.Name
@@ -98,11 +110,11 @@ for place in object_list:
     place.AddData(CasesData, DeathsData, FirstCase, FirstDeath, TotalCases, TotalDeaths)
     output["Data"].append(vars(place))
 
-with open('./src/assets/data.json', 'w') as output_file:
+with open("./src/assets/data.json", "w") as output_file:
     json.dump(output, output_file)
 
 try:
     shutil.rmtree(temp_folder)
 except OSError:
-    print (f"CSV File cleanup failed")
+    print(f"CSV File cleanup failed")
     exit(1)
